4f5703e161d5ab0073c09c7315d71ac9416f72ae
Saba Chokoshvili <saba.chokoshvili@ug.edu.ge>
2026-06-25 23:35:01 +0400

    Update MainApp.java

--- MainApp.java
+++ MainApp.java
@@ -1,5 +1,6 @@
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.*;
@@ -15,35 +16,39 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("საგამოცდო აპლიკაცია (უნივერსალური ვერსია)");
+        // 🌟 სათაური უფრო მოკლე და სუფთაა
+        primaryStage.setTitle("საგამოცდო აპლიკაცია");
 
+        // 🌟 ლეიბლები შევამცირეთ, რომ გვერდებზე არ გაიჭრას
         Label labelName = new Label("პროდუქტის სახელი:");
         TextField fieldName = new TextField();
 
-        Label labelVal1 = new Label("მონაცემი 1 (ფასი, კატეგორია ან თარიღი):");
+        Label labelVal1 = new Label("მონაცემი 1 (ტექსტი):");
         TextField fieldVal1 = new TextField();
 
-        Label labelVal2 = new Label("რაოდენობა (მხოლოდ მთელი რიცხვი):");
+        Label labelVal2 = new Label("მონაცემი 2 (რაოდენობა):");
         TextField fieldVal2 = new TextField();
 
         Button btnAdd = new Button("ბაზაში დამატება");
         
         GridPane grid = new GridPane();
-        grid.setHgap(10); grid.setVgap(10); grid.setPadding(new Insets(15));
+        grid.setHgap(10); 
+        grid.setVgap(12); 
+        grid.setPadding(new Insets(20));
+        grid.setAlignment(Pos.CENTER); // 🌟 ფორმა მოვაქციეთ ცენტრში
+        
         grid.add(labelName, 0, 0); grid.add(fieldName, 1, 0);
         grid.add(labelVal1, 0, 1); grid.add(fieldVal1, 1, 1);
         grid.add(labelVal2, 0, 2); grid.add(fieldVal2, 1, 2);
         grid.add(btnAdd, 1, 3);
 
         pieChart = new PieChart();
-        pieChart.setTitle("პროდუქტების განაწილება რაოდენობის მიხედვით");
+        pieChart.setTitle("სტატისტიკური განაწილება"); // 🌟 სათაური უფრო კომპაქტურია
 
         btnAdd.setOnAction(e -> {
             try {
                 String name = fieldName.getText();
-                String v1 = fieldVal1.getText(); // 🌟 შეიცვალა: პირდაპირ იღებს ტექსტს ყოველგვარი Parse-ის გარეშე!
-                
-                // მეორე მონაცემი აუცილებლად გადაგვაქვს რიცხვში ჩარტის მათემატიკისთვის
+                String v1 = fieldVal1.getText();
                 int v2 = Integer.parseInt(fieldVal2.getText());
 
                 Product p = new Product(name, v1, v2);
@@ -68,13 +73,15 @@ public class MainApp extends Application {
 
         updatePieChart();
 
-        VBox root = new VBox(20, grid, pieChart);
-        root.setPadding(new Insets(15));
-        primaryStage.setScene(new Scene(root, 480, 650));
+        VBox root = new VBox(25, grid, pieChart); // 🌟 დაშორება გავზარდეთ 25-მდე
+        root.setPadding(new Insets(20));
+        root.setAlignment(Pos.TOP_CENTER);
+
+        // 🌟 მნიშვნელოვანი ფიქსი: ფანჯარა გავხადეთ უფრო განიერი (550) და მაღალი (700)
+        primaryStage.setScene(new Scene(root, 550, 700));
         primaryStage.show();
     }
 
-    // 🌟 ეს მეთოდი 100%-ით ასრულებს ლექტორის პირობას: აჯამებს რეალურ რაოდენობებს
     private void updatePieChart() {
         try {
             List<Product> products = DatabaseManager.getAllProducts();
