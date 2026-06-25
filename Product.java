281f9f1178081a7f409be92b78323fba86abd56e
Saba Chokoshvili <saba.chokoshvili@ug.edu.ge>
2026-06-25 23:26:56 +0400

    Refactor Product class to use String for value1
    Changed value1 from double to String to accommodate new data type.

--- Product.java
+++ Product.java
@@ -1,27 +1,24 @@
 public class Product {
     private int id;
-    private String name;      // ტექსტური ველი (პროდუქტის სახელი, წიგნის სათაური და ა.შ.)
-    private double value1;    // რიცხვითი ველი 1 (ფასი, წონა, რეიტინგი)
-    private int value2;       // რიცხვითი ველი 2 (რაოდენობა, გვერდები, წელი)
+    private String name;
+    private String value1; // 🌟 შეიცვალა: გახდა String (double-ის ნაცვლად)
+    private int value2;    // დარჩა მთელი რიცხვი (int)
 
-    // კონსტრუქტორი ბაზიდან მონაცემების წასაკითხად
-    public Product(int id, String name, double value1, int value2) {
+    public Product(int id, String name, String value1, int value2) {
         this.id = id;
         this.name = name;
         this.value1 = value1;
         this.value2 = value2;
     }
 
-    // კონსტრუქტორი ბაზაში ახალი მონაცემის ჩასაწერად
-    public Product(String name, double value1, int value2) {
+    public Product(String name, String value1, int value2) {
         this.name = name;
         this.value1 = value1;
         this.value2 = value2;
     }
 
-    // გეთერები (Getters) - აუცილებელია Stream API-სთვის
     public int getId() { return id; }
     public String getName() { return name; }
-    public double getValue1() { return value1; }
+    public String getValue1() { return value1; } // 🌟 შეიცვალა: აბრუნებს String-ს
     public int getValue2() { return value2; }
 }
